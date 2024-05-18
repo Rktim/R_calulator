a3f81d6ee9db6318d4a3051ef0b5bda7fefe21ad
RAKT1M004 <147612260+RAKT1M004@users.noreply.github.com>
2024-05-18 16:30:58 +0530

    initial commit

---
+++ firt.r
@@ -0,0 +1,23 @@
+add <- function(x,y){
+    print(x + y)
+}
+
+subtract <- function(x,y){
+    print(x - y)
+}
+
+multiply <- function(x,y){
+    print(x * y)
+}
+
+divide <- function(x,y){
+    print(x / y)
+}
+
+a=2
+b=3
+
+add(a,b)
+subtract(a,b)
+multiply(a,b)
+divide(a,b)
